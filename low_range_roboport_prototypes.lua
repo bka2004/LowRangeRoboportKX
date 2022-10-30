c151ff9aa6055a0195a8498407a96ec64a18da3c
bka2004 <bk@anomalie.de>
2022-10-30 16:01:32 +0100

    initial idea doesn't work, instead several different prototypes

--- low_range_roboport_prototypes.lua
+++ low_range_roboport_prototypes.lua
@@ -1,42 +1,46 @@
 
 
 
-local CreateLRREntity = function()
+local CreateLRREntity = function(connectionRange)
 
     local entity = table.deepcopy(data.raw["roboport"]["roboport"])
 
-    entity.name = "lr-roboport"
-    entity.logistics_radius = 14
-    entity.construction_radius = 16
-    entity.logistics_connection_distance = 14
+    entity.name = "lr-roboport" + tostring(connectionRange)
+    entity.logistics_radius = connectionRange
+    entity.construction_radius = connectionRange + 2
+    entity.logistics_connection_distance = connectionRange
 
     return entity
 end
 
 
-local CreateLRRItem = function()
+local CreateLRRItem = function(connectionRange)
 
     local item = table.deepcopy(data.raw["item"]["roboport"])
 
-    item.name = "lr-roboport"
-    item.place_result = "lr-roboport"
+    item.name = "lr-roboport" + tostring(connectionRange)
+    item.place_result = "lr-roboport" + tostring(connectionRange)
 
     return item
 end
 
 
 
-local CreateLRRRecipe = function()
+local CreateLRRRecipe = function(connectionRange)
 
     local recipe = table.deepcopy(data.raw["recipe"]["roboport"])
 
     recipe.enabled = true
-    recipe.name = "lr-roboport"
-    recipe.result = "lr-roboport"
+    recipe.name = "lr-roboport" + tostring(connectionRange)
+    recipe.result = "lr-roboport" + tostring(connectionRange)
 
     return recipe
 end
 
 
 
-data:extend{CreateLRREntity(), CreateLRRItem(), CreateLRRRecipe()}
+for _, range in ipairs({8,10,12,14,16,18,20}) do
+    data:extend{CreateLRREntity(range), CreateLRRItem(range), CreateLRRRecipe(range)}
+    
+end
+
